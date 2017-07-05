388aca7b866e9cce436eef8cde1a3ae9a57dfdff
Greg Duncan <mail@gregduncan.co.uk>
2017-07-05 13:51:18 +0100

    Made pretty

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ class WebpackPrinter {
 		var isFirstCompile = true
 
 		compiler.plugin('done', stats => {
-            
 			clearConsole()
 
 			var messages = formatWebpackMessages(stats.toJson({}, true))
