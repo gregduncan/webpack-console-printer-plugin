01d1e917881ceb50bef19af1f3b1a4112f006745
Greg Duncan <mail@gregduncan.co.uk>
2017-07-05 13:48:23 +0100

    init

---
+++ index.js
@@ -0,0 +1,74 @@
+var chalk = require('chalk')
+
+var clearConsole = require('./utils/clearConsole')
+var formatWebpackMessages = require('./utils/formatWebpackMessages')
+var openBrowser = require('./utils/openBrowser')
+
+class WebpackPrinter {
+	constructor(options) {
+		options = options || {}
+		this.port = options.port || 8000
+		this.experimental = options.experimental || false
+	}
+
+	apply(compiler) {
+		let isFirstCompile = true
+
+		compiler.plugin('done', stats => {
+            
+			clearConsole()
+
+			const messages = formatWebpackMessages(stats.toJson({}, true))
+			const hasErrors = stats.hasErrors()
+			const hasWarnings = stats.hasWarnings()
+			const isSuccessful = !messages.errors.length && !messages.warnings.length
+			const showInstructions = isSuccessful && isFirstCompile
+
+			if (isSuccessful) {
+				console.log(chalk.green('Compiled successfully!'))
+			}
+
+			if (showInstructions) {
+				console.log()
+				console.log('The app is running on http://localhost:' + this.port)
+				console.log()
+				openBrowser('http://localhost:' + this.port + '/')
+				isFirstCompile = false
+			}
+
+			if (hasErrors) {
+				console.log(chalk.red('Failed to compile.'))
+				console.log()
+
+				if (this.experimental) {
+					messages.errors[0] = messages.errors[0].split('@')[0]
+				}
+
+				messages.errors.forEach(message => {
+					console.log(message)
+				})
+				return
+			}
+
+			if (hasWarnings) {
+				console.log(chalk.yellow('Compiled with warnings.'))
+				console.log()
+
+				if (this.experimental) {
+					messages.warnings[0] = messages.warnings[0].split('@')[0]
+				}
+
+				messages.warnings.forEach(message => {
+					console.log(message)
+				})
+				return
+			}
+		})
+
+		compiler.plugin('invalid', () => {
+			console.log(chalk.cyan('Loading'))
+		})
+	}
+}
+
+module.exports = WebpackPrinter
