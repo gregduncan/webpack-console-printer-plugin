c14fa7387e17aaa47257937533602579569eeac4
Greg Duncan <mail@gregduncan.co.uk>
2017-07-05 14:09:38 +0100

    Pretty config update

--- index.js
+++ index.js
@@ -5,69 +5,69 @@ var formatWebpackMessages = require('./utils/formatWebpackMessages')
 var openBrowser = require('./utils/openBrowser')
 
 class WebpackPrinter {
-	constructor(options) {
-		options = options || {}
-		this.port = options.port || 8000
-		this.experimental = options.experimental || false
-	}
+  constructor(options) {
+    options = options || {}
+    this.port = options.port || 8000
+    this.experimental = options.experimental || false
+  }
 
-	apply(compiler) {
-		var isFirstCompile = true
+  apply(compiler) {
+    var isFirstCompile = true
 
-		compiler.plugin('done', stats => {
-			clearConsole()
+    compiler.plugin('done', stats => {
+      clearConsole()
 
-			var messages = formatWebpackMessages(stats.toJson({}, true))
-			var hasErrors = stats.hasErrors()
-			var hasWarnings = stats.hasWarnings()
-			var isSuccessful = !messages.errors.length && !messages.warnings.length
-			var showInstructions = isSuccessful && isFirstCompile
+      var messages = formatWebpackMessages(stats.toJson({}, true))
+      var hasErrors = stats.hasErrors()
+      var hasWarnings = stats.hasWarnings()
+      var isSuccessful = !messages.errors.length && !messages.warnings.length
+      var showInstructions = isSuccessful && isFirstCompile
 
-			if (isSuccessful) {
-				console.log(chalk.green('Compiled successfully!'))
-			}
+      if (isSuccessful) {
+        console.log(chalk.green('Compiled successfully!'))
+      }
 
-			if (showInstructions) {
-				console.log()
-				console.log('The app is running on http://localhost:' + this.port)
-				console.log()
-				openBrowser('http://localhost:' + this.port + '/')
-				isFirstCompile = false
-			}
+      if (showInstructions) {
+        console.log()
+        console.log('The app is running on http://localhost:' + this.port)
+        console.log()
+        openBrowser('http://localhost:' + this.port + '/')
+        isFirstCompile = false
+      }
 
-			if (hasErrors) {
-				console.log(chalk.red('Failed to compile.'))
-				console.log()
+      if (hasErrors) {
+        console.log(chalk.red('Failed to compile.'))
+        console.log()
 
-				if (this.experimental) {
-					messages.errors[0] = messages.errors[0].split('@')[0]
-				}
+        if (this.experimental) {
+          messages.errors[0] = messages.errors[0].split('@')[0]
+        }
 
-				messages.errors.forEach(message => {
-					console.log(message)
-				})
-				return
-			}
+        messages.errors.forEach(message => {
+          console.log(message)
+        })
+        return
+      }
 
-			if (hasWarnings) {
-				console.log(chalk.yellow('Compiled with warnings.'))
-				console.log()
+      if (hasWarnings) {
+        console.log(chalk.yellow('Compiled with warnings.'))
+        console.log()
 
-				if (this.experimental) {
-					messages.warnings[0] = messages.warnings[0].split('@')[0]
-				}
+        if (this.experimental) {
+          messages.warnings[0] = messages.warnings[0].split('@')[0]
+        }
 
-				messages.warnings.forEach(message => {
-					console.log(message)
-				})
-				return
-			}
-		})
+        messages.warnings.forEach(message => {
+          console.log(message)
+        })
+        return
+      }
+    })
 
-		compiler.plugin('invalid', () => {
-			console.log(chalk.cyan('Loading...'))
-		})
-	}
+    compiler.plugin('invalid', () => {
+      console.log(chalk.cyan('Loading...'))
+    })
+  }
 }
 
 module.exports = WebpackPrinter
